13f850306c68e68db45f8e890e8a548e964017f9
OĞUZHAN MAVİ <mavioguz09@gmail.com>
2023-04-20 15:39:38 +0300

    springboot-security
    Java Springboot with Spring Security Application

---
+++ springboot-security-app/src/main/java/oguzhan/mavi/springbootsecurityapp/SpringbootSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package oguzhan.mavi.springbootsecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityAppApplication.class, args);
+	}
+
+}
---
+++ springboot-security-app/src/test/java/oguzhan/mavi/springbootsecurityapp/SpringbootSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package oguzhan.mavi.springbootsecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
